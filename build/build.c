13d18cc4bd4090d7a89469c19a2e1e87ea412b4b
Jesse <jbegin@protonmail.com>
2023-06-09 04:33:55 -0500

    update the build system to accomodate for later changes
    Updated the build system to accomodate for later changes, such as
    the ability to one day completely remove the terminal section of the
    program, and just use a GUI variant. For now, it is not possible to
    do this.

--- build/build.c
+++ build/build.c
@@ -1,3 +1,4 @@
+
 /*
  * Compile this file alongside lib/sbbs/sbbs.c using any C89 compatible 
  * compiler to produce a singular executable. This will produce a program
@@ -7,17 +8,63 @@
  * gcc -ansi -pedantic build.c ../lib/sbbs/sbbs.c -o build
  */
 
+#include <stdio.h>
 #include <stdlib.h>
 #include "../lib/sbbs/sbbs.h"
 
-int main(void) {
-	struct sbbsModule* main_module = NULL;
+extern size_t              stedGetNullTerminatedModuleListLength (struct sbbsModule** list); 
+extern struct sbbsModule** stedAddModuleToNullTerminatedList     (struct sbbsModule** list, struct sbbsModule* module); /* returns a pointer to the start of the list */
 
-	main_module = sbbsCreateModule("main_module");
+int main(int argc, char* argv[]) {
+
+	size_t i = 0;
+	static struct sbbsModule** os_dependent_modules = NULL;
+	static struct sbbsModule* main_module = NULL;
+	static struct sbbsModule* tmp_module = NULL; /* use this any time that you'd like to */
+
+	/* main module */
+	main_module = sbbsCreateModule("main");
 	sbbsAddModuleSource(main_module, "../src/main.c", NULL);
 
+	/* terminal module */
+	#ifdef __GLIBC__
+		tmp_module = sbbsCreateModule("glibc-terminal");
+		sbbsAddModuleSource(tmp_module, "../src/linux/terminal.c", NULL);
+		os_dependent_modules = stedAddModuleToNullTerminatedList(os_dependent_modules, tmp_module);
+	#else
+		#error "This platform is not supported by Sted. Please get in touch with the devs to see how long or if this platform will be supported"
+	#endif
+
+	/* make os dependent modules dependencies of the main module */
+	for (i = 0; os_dependent_modules[i] != NULL; i++) {
+		sbbsAddModuleDependency(main_module, os_dependent_modules[i]);
+	}
+
 	sbbsCompileModule(main_module);
-	sbbsCreateExecutable(main_module, "editor", NULL);
+	sbbsCreateExecutable(main_module, "sted", NULL);
+
+	sbbsDestroyModule(main_module);
+	free(os_dependent_modules);
 
 	return 0;
+}
+
+size_t stedGetNullTerminatedModuleListLength(struct sbbsModule** list) {
+	size_t length = 0, i = 0;
+	for (i = 0; list[i] != NULL; i++) {
+		length++;
+	}
+	return length;
+}
+
+struct sbbsModule** stedAddModuleToNullTerminatedList(struct sbbsModule** list, struct sbbsModule* module) {
+	if (list == NULL) {
+		list = calloc(2, sizeof(struct sbbsModule*));
+		list[0] = module;
+		list[1] = NULL;
+	} else {
+		const size_t LIST_LEN = stedGetNullTerminatedModuleListLength(list) + 2;
+		list = realloc(list, LIST_LEN * sizeof(struct sbbsModule*));
+	}
+	return list;
 }
